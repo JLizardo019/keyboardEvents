90716fa79b56cdc4ac60166e32d231742dcaee09
JLizardo019 <jal879@nyu.edu>
2024-07-18 10:10:12 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let myXPos = 100;
+let myYPos = 100;
+let marioImage;
+
+function preload(){
+    marioImage = loadImage("https://upload.wikimedia.org/wikipedia/en/a/a9/MarioNSMBUDeluxe.png");
+}
+
+function setup(){
+    createCanvas(500,500);
+    noStroke();
+
+    rectMode(CENTER); // p5 use the x,y for rect to refer to the center point
+
+}
+
+function draw(){
+    background(0);
+
+    fill(255,0,0);
+    //rect(myXPos, myYPos, 50,50);
+    image(marioImage, myXPos, myYPos,50,50);
+
+    if(keyIsDown(LEFT_ARROW)){
+        myXPos -= 3;
+    }
+
+    if(keyIsDown(RIGHT_ARROW)){
+        myXPos += 3;
+    }
+
+    if(keyIsDown(UP_ARROW)){
+        myYPos -= 3;
+    }
+
+    if(keyIsDown(DOWN_ARROW)){
+        myYPos += 3;
+    }
+
+
+
+}
